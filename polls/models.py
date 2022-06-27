e54148dc72a9623f652df880c15f090eaf3acb32
Tony Obriku <tonyobriku@gmail.com>
2022-06-27 05:20:36 +0100

    commit includes alot of changes involving the model and also registering it on the admin site

---
+++ polls/models.py
@@ -0,0 +1,23 @@
+from secrets import choice
+from django.db import models
+
+# Create your models here.
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=25)
+    question_description = models.CharField(max_length=266, default="Basic Description")
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.question_text
+
+
+class Choices(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_answer = models.CharField(max_length=20)
+    votes = models.IntegerField(default=0)
+
+
+    def __str__(self):
+        return f"{self.question} ---> {self.choice_answer}"
